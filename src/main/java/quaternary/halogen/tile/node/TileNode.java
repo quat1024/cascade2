e18740bb756b4f6a5eb93ec0bb926fa910f5c6a0
quat <scratch933@gmail.com>
2017-10-11 16:34:34 -0400

    Test commit to see how renaming repos works. Uh oh

--- src/main/java/quaternary/halogen/tile/node/TileNode.java
+++ src/main/java/quaternary/halogen/tile/node/TileNode.java
@@ -1,5 +1,6 @@
 package quaternary.halogen.tile.node;
 // Comment with space after the slashes to appease Nerxit.
+//Hey nerxiepoo shut up about my comment habits k
 
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
@@ -8,6 +9,8 @@ import quaternary.halogen.util.DisgustingNumbers;
 
 public class TileNode extends TileEntity implements ITickable {
 	AuraStorageCap storageCap = new AuraStorageCap(DisgustingNumbers.NODE_MAX_AURA);
+	
+	//test
 	
 	@Override
 	public void update() {
