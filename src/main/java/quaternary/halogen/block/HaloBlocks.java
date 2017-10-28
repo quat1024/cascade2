4ee50ca88e8032c9da83ee406ed61949e8a02398
quat <scratch933@gmail.com>
2017-10-28 01:47:43 -0400

    Moon stone subtype stuff. Oh look moon stone is broken. Isn't that fun

--- src/main/java/quaternary/halogen/block/HaloBlocks.java
+++ src/main/java/quaternary/halogen/block/HaloBlocks.java
@@ -1,17 +1,18 @@
 package quaternary.halogen.block;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
 import quaternary.halogen.block.node.BlockNode;
+import quaternary.halogen.block.plain.BlockMoonStone;
 
 public class HaloBlocks {
 	public static final HaloBlock[] blocks = {
 					new BlockNode("node_white"),
-					new HaloBlock("moon_stone", Material.ROCK),
-					new HaloBlock("polished_moon_stone", Material.ROCK)
+					new BlockMoonStone()
+					//new HaloBlock("moon_stone", Material.ROCK),
+					//new HaloBlock("polished_moon_stone", Material.ROCK)
 	};
 	
 	public static void registerBlocks(IForgeRegistry<Block> reg) {
